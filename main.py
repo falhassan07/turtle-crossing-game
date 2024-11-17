9332fd3c926b112c74c7d552d98a4df4b1a1b827
Farouk Alhassan <omarfarouk@farouks-mbp.home>
2024-11-17 01:59:19 -0500

    modified main.py

--- main.py
+++ main.py
@@ -8,7 +8,9 @@ from scoreboard import Scoreboard
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
+
 player = Player()
+car_manager = CarManager()
 scoreboard = Scoreboard()
 
 screen.listen()
@@ -18,11 +20,23 @@ game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+    car_manager.create_car()
+    car_manager.move_cars()
 
-
+    #detect successful crossing
     if player.ycor() > 280:
         player.reset_position()
+        car_manager.increase_speed()
         scoreboard.increase_level()
+
+    #detect collision with car
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+            scoreboard.game_over()
+
+    
+        
 
 
 
