3f130a6b11fc863d7c65dd7b582d206a9ebb2b4b
Farouk Alhassan <omarfarouk@farouks-mbp.home>
2024-11-17 01:08:13 -0500

    added starter files and some functionalilities

---
+++ scoreboard.py
@@ -0,0 +1,26 @@
+from turtle import Turtle
+FONT = ("Courier", 24, "normal")
+
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.penup()
+        self.hideturtle()
+        self.color("black")
+        self.goto(-270,270)
+        self.level = 1
+        self.update_level()
+
+
+    def increase_level(self):
+        self.level = self.level + 1
+        self.update_level()
+
+    
+
+    def update_level(self):
+        self.clear()
+        self.write(f"Level: {self.level}", font=(FONT))
+
+
